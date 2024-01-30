c12902b3efed7c661f6cb35d416f2b68654cdeb1
Cecep Cahyana <cecepwahyu.2019@student.uny.ac.id>
2024-01-30 08:43:53 +0700

    first commit

---
+++ untitled/src/RecursiveMethod.java
@@ -0,0 +1,16 @@
+public class RecursiveMethod {
+    public static void main(String[] args) {
+        System.out.println(factorialLoop(5));
+    }
+
+    static int factorialLoop(int value) {
+        var result = 1;
+
+        for (var counter = 1; counter <= value; counter++) {
+            result *= counter;
+        }
+
+        return  result;
+    }
+
+}
---
+++ untitled/src/RecursiveMethod2.java
@@ -0,0 +1,23 @@
+public class RecursiveMethod2 {
+    public static void main(String[] args) {
+        System.out.println(factorialRecursive(8));
+    }
+
+    /*
+    *s
+    *
+    * ss
+    * s
+    * s
+    * ss
+    *
+     */
+    static int factorialRecursive(int value) {
+        if (value == 1) {
+            return 1;
+        } else {
+            return value * factorialRecursive(value-1);
+        }
+    }
+
+}

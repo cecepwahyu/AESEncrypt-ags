354876b32c45665cf13a30de2c48903d56d5492f
Cecep Cahyana <cecepwahyu.2019@student.uny.ac.id>
2024-02-13 10:54:31 +0700

    feat: MAP

---
+++ untitled/src/programmer/zaman/now/application/HashMapApp.java
@@ -0,0 +1,18 @@
+package programmer.zaman.now.application;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapApp {
+    public static void main(String[] args) {
+
+        //MAP
+        Map<String, String> map = new HashMap<>();
+        map.put("name.first", "Cecep");
+        map.put("name.middle", "Wahyu");
+        map.put("name.last", "Cahyana");
+
+        System.out.println(map.get("name.first"));
+
+    }
+}
